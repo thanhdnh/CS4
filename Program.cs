1c9ac07c32311c28b0550aba6dcc814aa92ce05a
thanhdnh <myhoangthanh@yahoo.com>
2022-10-22 15:20:00 +0700

    OKK

--- Program.cs
+++ Program.cs
@@ -32,12 +32,57 @@
             System.Console.WriteLine();
         }
     }
+    static void PrintUnderArray2D(Array arr){
+        for(int i=arr.GetLowerBound(0); i<=arr.GetUpperBound(0); i++){
+            for(int j=arr.GetLowerBound(1); j<=arr.GetUpperBound(1); j++)
+                if(i>=j)
+                    System.Console.Write("{0, 12}", arr.GetValue(i, j));
+                else
+                    System.Console.Write("{0, 12}", "");
+            System.Console.WriteLine();
+        }
+    }
+    static void PrintUnderArray2D2(Array arr){
+        for(int i=arr.GetLowerBound(0); i<=arr.GetUpperBound(0); i++){
+            for(int j=arr.GetLowerBound(1); j<=arr.GetLowerBound(1) + i -1; j++)
+                    System.Console.Write("{0, 12}", arr.GetValue(i, j));
+            System.Console.WriteLine();
+        }
+    }
+    static void GenerateList1D<T>(List<T> list, int times){
+        Random r = new Random();
+        for(int i=0; i<times; i++)
+            list.Add((dynamic)r.Next(1, 10)*10);
+    }
+    static void PrintList1D<T>(List<T> list){
+        /*for(int i=0; i<list.Count; i++)
+            System.Console.Write("{0, 5}", list[i]);*/
+        foreach(T v in list)//T hoac dynamic hoac var
+            System.Console.Write("{0, 5}", v);
+    }
+    static T SumList<T>(List<T> list){
+        T sum = list[0];
+        for(int i=1; i<list.Count; i++)
+            sum += (dynamic)list[i];
+        /*foreach(T v in list)
+            sum += (dynamic)v;
+        sum -= (dynamic)list[0];*/
+        return sum;
+    }
     static void Main(string[] args){
         Console.Clear();
 
-        Array arr;
+        List<float> list = new List<float>();
+        GenerateList1D<float>(list, 5);
+        PrintList1D<float>(list);
+        System.Console.WriteLine("\nSum List1D: {0}", SumList<float>(list));
+
+        /*Array arr;
         GenerateArray2D(out arr, 5, 5, 1, 1);
         PrintArray2D(arr);
+        System.Console.WriteLine();
+        PrintUnderArray2D(arr);
+        PrintUnderArray2D2(arr);*/
 
         /*Array arr;
         GenerateArray1D(out arr, 5, 1);
