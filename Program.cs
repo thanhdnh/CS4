c13c31c87d1c3b2c1f34727eedc151e8c951d82e
thanhdnh <myhoangthanh@yahoo.com>
2022-10-29 15:36:59 +0700

    OKKKK

--- Program.cs
+++ Program.cs
@@ -67,15 +67,72 @@ class Program
       return d;
     }
   }
+  static int SumStack(MyStack ms)
+  {
+    MyStack temp = new MyStack();
+    int sum = 0;
+    while (!ms.IsEmpty())
+    {
+      int n = (int)ms.Pop().data;
+      sum += n;
+      temp.Push(n);
+    }
+    while (!temp.IsEmpty())
+      ms.Push((int)temp.Pop().data);
+    return sum;
+  }
+  static int SumQueue(MyQueue mq)
+  {
+    int sum = 0;
+    MyQueue tempQueue = new MyQueue();
+    while (!mq.IsEmpty())
+    {
+      int t = (int)mq.Dequeue().data;
+      sum += t;
+      tempQueue.Enqueue(t);
+    }
+    while (!tempQueue.IsEmpty())
+    {
+      mq.Enqueue(tempQueue.Dequeue());
+    }
+    return sum;
+  }
+  static int FindS(MyStack ms, int x)
+  {
+    MyStack temp = new MyStack();
+    int vitri = -1;
+    int chiso = 0;
+    while (!ms.IsEmpty())
+    {
+      int n = (int)ms.Pop().data;
+      if (n == x) { vitri = chiso; }
+      else chiso += 1;
+      temp.Push(n);
+    }
+    while (!temp.IsEmpty())
+    {
+      ms.Push((int)temp.Pop().data);
+    }
+    return vitri;
+  }
+  static int FindQ(MyQueue mq, int x)
+  {
+    return 0;
+  }
   static void Main(string[] args)
   {
     Console.Clear();
+
+    MyStack ms = new MyStack();
+    ms.Push(1); ms.Push(2); ms.Push(3);
+    Console.WriteLine(FindS(ms, 2));
+    System.Console.WriteLine("Sum of Stack: {0}", SumStack(ms));
 
     MyQueue mq = new MyQueue();
     mq.Enqueue(1);
     mq.Enqueue(2);
     mq.Enqueue(3);
-
+    System.Console.WriteLine("Sum of Queue: {0}", SumQueue(mq));
     Console.ReadLine();
   }
 }
